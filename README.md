cdc19d9129521da4382cd1de516b3c0ade1500c2
Thiago Henrique <thiago.h.silva.98@gmail.com>
2019-11-13 22:25:31 -0200

    Inital commit 🎉

---
+++ README.md
@@ -0,0 +1,10 @@
+# Projeto Laboratório de Banco de Dados
+
+Esse projeto visa a criação de um sisteminha web utilizando as 3 camadas(front,back e bd).
+
+As tecnologias utilizadas são: React no front, node no back e mySql no bano.
+
+## Estrutura de pastas
+* **server/** : Contem a modulo que inicia o servidor
+* **db/** : Contem um script SQL para gerar um banco com dados iniciais.
+* **client/** : Contem todo o código da aplicação front-end
---
+++ server/index.js
@@ -0,0 +1,43 @@
+const path    = require('path');
+const mysql = require('mysql');
+const express = require('express');
+const app     = express();
+const router  = express.Router();
+
+const con = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'alunofatec',
+  database : 'estoque'
+});
+
+con.connect((err)=>{
+  	if (err) throw err;
+  	console.log("Connected!");
+});
+
+router.get('/api/categoria',function(req,res){
+	con.query("SELECT * FROM CATEGORIA", (error, result) =>{
+    	if (error) throw error;
+    	
+    	res.send({
+    		categorias: result.map(categoria=>({
+    			id: categoria.CodCategoria,
+    			nome: categoria.NomeCategoria
+    		}))
+    	});
+  	});
+});
+
+router.get('/',function(req,res){
+	res.sendFile(path.resolve(__dirname, '../client/index.html'));
+});
+
+router.get('/*',function(req,res){
+	res.redirect('/');
+});
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 3000);
+console.log('Running at Port 3000');
