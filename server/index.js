c4099c4ccae0aa724e1d893367f5e3e946b068ac
Thiago Henrique <thiago.h.silva.98@gmail.com>
2019-11-18 21:59:18 -0200

    rotas de get de FORNECEDOR

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const router  = express.Router();
 const con = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : '',
+  password : 'alunofatec',
   database : 'estoque'
 });
 
@@ -66,6 +66,83 @@ router.put('/api/categoria/:id',function(req,res){
 		  });
   	});
 });
+
+//CIDADE
+router.get('/api/cidade',function(req,res){
+	con.query('SELECT * FROM CIDADE', (error, result)=>{
+		if (error) throw error;
+
+		res.send(result);
+	});
+
+});
+
+router.get('/api/cidade/:id',function(req,res){
+	con.query(`SELECT * FROM CIDADE WHERE CodCidade=${req.params.id}`, (error,result) =>{
+		if (error) throw error;
+
+		res.send(result);
+		
+  	});
+});
+
+router.post('/api/cidade/',function(req,res){
+	const cidadeReq = req.body;
+	con.query(`INSERT INTO CIDADE (Cidade,Uf) VALUES ('${cidadeReq.nome}','${cidadeReq.uf}')`, (error, result) =>{
+    	if (error) throw error;
+
+		con.query(`SELECT * FROM CIDADE WHERE CodCidade=${result.insertId}`, (selectError, selectResult) =>{
+			if (selectError) throw selectError;
+			
+			res.send(selectResult);
+		  });
+  	});
+});
+
+router.delete('/api/cidade/:id',function(req,res){
+	con.query(`DELETE FROM CIDADE WHERE CodCidade=${req.params.id}`, (error, result) =>{
+    	if (error) throw error;
+    	
+    	res.send({
+			result: 'OK'
+		});
+  	});
+});
+
+router.put('/api/cidade/:id',function(req,res){
+	const categoriaReq = req.body;
+	con.query(`
+		UPDATE CIDADE
+		SET  Cidade,uf='${categoriaReq.nome}','${categoriaReq.uf}'
+		WHERE CodCidade=${req.params.id}`, 
+		(error, result) =>{
+    	if (error) throw error;
+		con.query(`SELECT * FROM CIDADE WHERE CodCidade=${req.params.id}`, (selectError, selectResult) =>{
+			if (selectError) throw selectError;
+			res.send(selectResult);
+		  });
+  	});
+});
+
+//FORNECEDOR
+router.get('/api/fornecedor',function(req,res){
+	con.query('SELECT * FROM FORNECEDOR', (error, result)=>{
+		if (error) throw error;
+
+		res.send(result);
+	});
+
+});	
+
+router.get('/api/fornecedor/:id',function(req,res){
+	con.query(`SELECT * FROM FORNECEDOR WHERE CodFornecedor=${req.params.id}`, (error,result) =>{
+		if (error) throw error;
+
+		res.send(result);
+		
+  	});
+});
+
 
 //everything else
 router.get('/*',function(req,res){
